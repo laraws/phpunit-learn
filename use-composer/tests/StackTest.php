3bada5322ab73ad3984c31d00550ef95ada40f09
hasee-linux <larawsw@outlook.com>
2020-04-13 15:15:18 +0800

    feat: depend test

--- use-composer/tests/StackTest.php
+++ use-composer/tests/StackTest.php
@@ -3,17 +3,34 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
-    public function testPushAndPop()
+    public function testEmpty()
     {
         $stack = [];
-        $this->assertSame(0, count($stack));
+        $this->assertEmpty($stack);
 
-        array_push($stack, 'foo');
-        $this->assertSame('foo', $stack[count($stack) - 1]);
-        $this->assertSame(1, count($stack));
-
-        $this->assertSame('foo', array_pop($stack));
-        $this->assertSame(0, count($stack));
+        return $stack;
 
     }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(array $stack)
+    {
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack) - 1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     */
+    public function testPop(array $stack)
+    {
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+    }
+
 }
