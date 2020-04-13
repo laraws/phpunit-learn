2e1ccdbc305c7e9a0fe187c8f47db8072398c148
hasee-linux <larawsw@outlook.com>
2020-04-13 15:39:35 +0800

    feat: update DataTest to add array key

--- use-composer/tests/DataTest.php
+++ use-composer/tests/DataTest.php
@@ -11,13 +11,23 @@ class DataTest extends TestCase
         $this->assertSame($expected, $a + $b);
     }
 
+//    public function additionProvider()
+//    {
+//        return [
+//            [0, 0, 0],
+//            [0, 1, 1],
+//            [1, 0, 1],
+//            [1, 1, 3]
+//        ];
+//    }
+
     public function additionProvider()
     {
         return [
-            [0, 0, 0],
-            [0, 1, 1],
-            [1, 0, 1],
-            [1, 1, 3]
+            'adding zeros'  => [0, 0, 0],
+            'zero plus one' => [0, 1, 1],
+            'one plus zero' => [1, 0, 1],
+            'one plus one'  => [1, 1, 3]
         ];
     }
 }
